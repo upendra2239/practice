dd8fc70b30ca180e1907b7c2bf09d85c5d9d2140
upendra reddy <upendrareddyvagalla@gmail.com>
2024-11-04 21:00:06 +0530

    Revert "Add files via upload"

--- SpringCrud/SpringCrud/src/main/java/com/example/SpringCrud/Repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SpringCrud.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.SpringCrud.Entity.CustomerDetails;
-
-public interface CustomerRepository extends JpaRepository<CustomerDetails, Long>{
-
-}
